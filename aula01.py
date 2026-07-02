f66f4c001852ef189c842733bd2505367bf0fcb2
godoyeduardo135 <godoyeduardo135@gmail.com>
2026-07-02 16:41:19 -0300

    feat: adiciona notebook da aula de análise de dados

---
+++ aula01.py
@@ -0,0 +1,11 @@
+#O comando print, exibe uma informação na tela do usuário.
+idade = 30 #Idade um
+Idade_dois = '30' #Idade dois 
+print(idade)
+print(Idade_dois)
+
+Idade_futura = idade + 1
+print(Idade_futura)
+
+print(type(idade))
+print(type(Idade_dois))
